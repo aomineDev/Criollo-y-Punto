5bc3fe7589dc1e8f089bfcebc3470ff29b224a7b
Jhordan <calixto4211@gmail.com>
2025-06-27 22:13:16 -0500

    componente footer terminado

---
+++ js/footer/components/Footer.js
@@ -0,0 +1,45 @@
+export function Footer() {
+
+    return /*html*/ `
+        <div class="footer__kitchen">
+            <img class="kitchen-base" src="img/banner/footer3.png" alt="#">
+            <img class="plato plato--1" src="img/banner/plato1.png" alt="#">
+            <img class="plato plato--2" src="img/banner/plato2.png" alt="#">
+            <img class="plato plato--3" src="img/banner/plato3.png" alt="#">
+            <img class="plato plato--4" src="img/banner/plato4.png" alt="#">
+            <img class="plato plato--5" src="img/banner/plato5.png" alt="#">
+        </div>
+
+        <div class="footer__content">
+
+            <div class="content__redes footer__box">
+                <a href=""><img src="img/logo/logo-blanco.png" alt=""></a>
+                <div class="social-media">
+                <a href="https://facebook.com/"><i class="fa-brands fa-square-facebook"></i></a>
+                <a href="https://www.instagram.com/"><i class="fa-brands fa-square-instagram"></i></a>
+                <a href=""><i class="fa-brands fa-square-x-twitter"></i></a>
+                <a href=""><i class="fa-brands fa-tiktok"></i></a>
+                </div>
+            </div>
+
+            <div class="content__contact footer__box">
+                <h4 class="title-4">Contactanos</h4>
+                <p>+51 923745971</p>
+                <p>restauranteCriollo&Punto@gmail.com</p>
+            </div>
+
+            <div class="content__redes footer__box">
+                <h4 class="title-4">Direccion</h4>
+                <p>Av. Los manzanos 123</p>
+            </div>
+
+            <div class="content__atention footer__box">
+                <h4 class="title-4">Horario de Atención</h4>
+                <p>Lunes - Sábado: <br> 3 PM - 10 PM</p>
+                <p>Domingo: <br> Cerrado</p>
+            </div>
+
+        </div>
+    `
+
+}
