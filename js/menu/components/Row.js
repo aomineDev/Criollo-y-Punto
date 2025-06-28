558b7fea9ad402d3c616e302847c7be822ac46dc
Jhordan <calixto4211@gmail.com>
2025-06-28 02:08:58 -0500

    avance menu

---
+++ js/menu/components/Row.js
@@ -0,0 +1,18 @@
+export function Row({ id, image, name, price, description }) {
+
+    return /*html*/ `
+        <tr class="table__row">
+            <td><img src="img/Delivery/${image}" alt=""></td>
+            <td>
+                <p>${name}</p>
+            </td>
+            <td>
+                <p>${description}</p>
+            </td>
+            <td>
+                <p>S/. ${price}</p>
+            </td>
+        </tr>
+    `
+
+}
